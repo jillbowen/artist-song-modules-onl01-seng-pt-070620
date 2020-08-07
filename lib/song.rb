96fe673699cd51bf60581ff00ebec5cdcdd08896
Jill Short <jillian.b.short@gmail.com>
2020-08-07 23:18:04 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,7 +2,6 @@ require 'pry'
 
 class Song
   extend Memorable::ClassMethods, Findable
-  #extend Findable
   include Paramable, Memorable::InstanceMethods
   attr_accessor :name
   attr_reader :artist
